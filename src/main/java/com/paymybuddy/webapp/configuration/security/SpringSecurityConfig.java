10f55145375c96086a948e1bbebf4ae75df32115
Michael "Steezy" A <38781991+steezycoding@users.noreply.github.com>
2024-09-18 13:45:52 +0200

    :sparkles: Add custom Login handling

--- src/main/java/com/paymybuddy/webapp/configuration/security/SpringSecurityConfig.java
+++ src/main/java/com/paymybuddy/webapp/configuration/security/SpringSecurityConfig.java
@@ -3,7 +3,6 @@ package com.paymybuddy.webapp.configuration.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,11 +21,21 @@ public class SpringSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/swagger-ui/**", "v3/api-docs/**", "/actuator/**").permitAll()
-						.anyRequest().authenticated()
+		return http
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/css/**").permitAll()
+						.requestMatchers("/swagger-ui/**", "v3/api-docs/**", "/actuator/**").hasRole("ADMIN")
+						.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.defaultSuccessUrl("/", true)
+						.permitAll())
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.logoutSuccessUrl("/login")
+						.invalidateHttpSession(true)
+						.deleteCookies("JSESSIONID")
 				)
-				.formLogin(Customizer.withDefaults())
 				.build();
 	}
 
