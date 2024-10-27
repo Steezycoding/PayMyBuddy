84aedf8c38939411d4cfee6824350145229b4926
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2024-10-27 18:19:13 +0100

    Handle a custom Login page
    * :sparkles: Add initial (Spring Security role based) Login feature
    
    * :lipstick: Add main app page (dynamic)
    
    * :sparkles: Add custom Login handling
    
    * :wrench: <SonarCloud> fix Jacoco path

--- src/main/java/com/paymybuddy/webapp/configuration/security/SpringSecurityConfig.java
+++ src/main/java/com/paymybuddy/webapp/configuration/security/SpringSecurityConfig.java
@@ -2,23 +2,54 @@ package com.paymybuddy.webapp.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-
 public class SpringSecurityConfig {
+
+	private final CustomUserDetailsService customUserDetailsService;
+
+	public SpringSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+		this.customUserDetailsService = customUserDetailsService;
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/swagger-ui/**", "v3/api-docs/**", "/actuator/**").permitAll()
-						.anyRequest().authenticated()
+		return http
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/css/**").permitAll()
+						.requestMatchers("/swagger-ui/**", "v3/api-docs/**", "/actuator/**").hasRole("ADMIN")
+						.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.defaultSuccessUrl("/", true)
+						.permitAll())
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.logoutSuccessUrl("/login")
+						.invalidateHttpSession(true)
+						.deleteCookies("JSESSIONID")
 				)
-				.formLogin(Customizer.withDefaults())
 				.build();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder
+				.userDetailsService(customUserDetailsService)
+				.passwordEncoder(bCryptPasswordEncoder);
+		return authenticationManagerBuilder.build();
 	}
 }
