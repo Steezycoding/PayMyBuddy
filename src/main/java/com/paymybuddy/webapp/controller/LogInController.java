c76aa28c805df351050a795a78f0f5b77e1aee65
Michael "Steezy" A <38781991+Steezycoding@users.noreply.github.com>
2025-01-11 14:55:57 +0100

    :memo: Added OpenAPI documentation on LogInController

--- src/main/java/com/paymybuddy/webapp/controller/LogInController.java
+++ src/main/java/com/paymybuddy/webapp/controller/LogInController.java
@@ -1,5 +1,7 @@
 package com.paymybuddy.webapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/login")
 public class LogInController {
+	@Operation(summary = "Get the login page", description = "Returns the login page")
+	@ApiResponse(responseCode = "200", description = "Login page loaded successfully")
 	@GetMapping
 	public String loginPage(Model model) {
 		model.addAttribute("title", "Login");
